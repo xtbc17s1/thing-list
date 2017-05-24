fa024c06f58e9e3e9076b4e17c1e674faa827f17
Dave Strus <dave@getfretless.com>
2017-05-24 09:59:45 -0400

    Remove hard-coded things

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import AddThingButton from './AddThingButton'
 
 class App extends Component {
   state = {
-    things: {
-      'thing-1': { id: 'thing-1', name: 'Milk' },
-      'thing-2': { id: 'thing-2', name: 'Bread' },
-      'thing-3': { id: 'thing-3', name: 'Bibb lettuce' },
-    }
+    things: {}
   }
 
   thing() {

a5befd00fef9944bf3420bc2313fb5b978a88a1a
Dave Strus <dave@getfretless.com>
2017-05-24 15:27:04 -0400

    Convert Thing to a class

--- src/Thing.js
+++ src/Thing.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContentEditable from 'react-contenteditable'
 
 import './Thing.css'
 import Actions from './Actions'
 
-const Thing = ({ thing, saveThing, removeThing }) => {
-  const updateName = (ev) => {
+class Thing extends Component {
+  updateName = (ev) => {
+    const { thing, saveThing } = this.props
     thing.name = ev.target.value
     saveThing(thing)
   }
 
-  return (
-    <li className="Thing">
-      <input type="checkbox" value="on" />
-      <div className="details">
-        <ContentEditable
-          className="name"
-          html={thing.name}
-          onChange={updateName}
-        />
-        <Actions thing={thing} removeThing={removeThing} />
-      </div>
-    </li>
-  )
+  render() {
+    const { thing, removeThing } = this.props
+
+    return (
+      <li className="Thing">
+        <input type="checkbox" value="on" />
+        <div className="details">
+          <ContentEditable
+            className="name"
+            html={thing.name}
+            onChange={this.updateName}
+          />
+          <Actions thing={thing} removeThing={removeThing} />
+        </div>
+      </li>
+    )
+  }
 }
 
 export default Thing

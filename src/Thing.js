f075cb4da2fea7e6261da98ddf2b0cb3658e7e78
Dave Strus <dave@getfretless.com>
2017-05-24 15:31:58 -0400

    Autofocus new things

--- src/Thing.js
+++ src/Thing.js
@@ -5,6 +5,10 @@ import './Thing.css'
 import Actions from './Actions'
 
 class Thing extends Component {
+  componentDidMount() {
+    this.nameInput.htmlEl.focus()
+  }
+
   updateName = (ev) => {
     const { thing, saveThing } = this.props
     thing.name = ev.target.value
@@ -22,6 +26,7 @@ class Thing extends Component {
             className="name"
             html={thing.name}
             onChange={this.updateName}
+            ref={input => this.nameInput = input}
           />
           <Actions thing={thing} removeThing={removeThing} />
         </div>

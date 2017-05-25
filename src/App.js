efb6a02f0cb8aaeef7ccd95c8bfe4cbd03cfa909
Dave Strus <dave@getfretless.com>
2017-05-25 14:41:57 -0400

    Add placeholder signedIn method

--- src/App.js
+++ src/App.js
@@ -51,22 +51,33 @@ class App extends Component {
     this.setState({ things })
   }
 
-  render() {
+  signedIn = () => {
+    return true
+  }
+
+  renderMain = () => {
     const actions = {
       saveThing: this.saveThing,
       removeThing: this.removeThing,
     }
 
     return (
-      <div className="App">
-        <Header />
-        <SignIn />
+      <div>
         <SignOut />
         <AddThingButton addThing={this.addThing} />
         <ThingList
           things={this.state.things}
           {...actions}
         />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        { this.signedIn() ? this.renderMain() : <SignIn /> }
       </div>
     );
   }

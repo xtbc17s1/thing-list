f83454c4dc31f22a39c831b5ab98f35533e2ee89
Dave Strus <dave@getfretless.com>
2017-05-25 14:23:59 -0400

    Add due dates to Things

--- src/Thing.js
+++ src/Thing.js
@@ -11,9 +11,10 @@ class Thing extends Component {
     }
   }
 
-  updateName = (ev) => {
+  handleChange = (ev) => {
     const { thing, saveThing } = this.props
-    thing.name = ev.target.value
+    const field = ev.currentTarget.getAttribute('name')
+    thing[field] = ev.target.value
     saveThing(thing)
   }
 
@@ -43,10 +44,17 @@ class Thing extends Component {
         <div className="details">
           <ContentEditable
             className="name"
+            name="name"
             html={thing.name}
-            onChange={this.updateName}
+            onChange={this.handleChange}
             onKeyPress={this.blurOnEnter}
             ref={input => this.nameInput = input}
+          />
+          <input
+            type="date"
+            name="dueOn"
+            defaultValue={thing.dueOn}
+            onChange={this.handleChange}
           />
           <Actions thing={thing} removeThing={removeThing} />
         </div>
